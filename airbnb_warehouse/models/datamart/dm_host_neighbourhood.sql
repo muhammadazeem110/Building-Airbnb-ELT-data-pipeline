d40e0c0cbf79ace1b3d771db3b9413b9b764069e
Muhammad Azeem <azeembinmaqsood@gmail.com>
2025-06-10 12:50:06 +0500

    update the silver modeling diagram and add creating table file

--- airbnb_warehouse/models/datamart/dm_host_neighbourhood.sql
+++ airbnb_warehouse/models/datamart/dm_host_neighbourhood.sql
@@ -1,8 +1,14 @@
 SELECT
     lga_code as host_neighbourhood_lga,
     DATE_TRUNC('month', scraped_date) as month,
+
+    -- Number of distinct host
     COUNT(DISTINCT host_id) as host_id,
+
+    -- Estimated Revenue
     ROUND(SUM(estimated_revenue), 2) as estimated_revenue,
+
+    -- Estimated Revenue per host (distinct)
     ROUND(SUM(estimated_revenue) / NULLIF(COUNT(DISTINCT host_id), 0), 2) AS revenue_per_host
 FROM {{ref("fact_airbnb_listing")}} as fal
 JOIN {{ref("dim_lga")}} as lga
